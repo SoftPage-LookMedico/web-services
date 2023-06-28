5cd489f85e4d06c99c7ede0182ed364b5b8250cc
ValeryAyasta <102206366+ValeryAyasta@users.noreply.github.com>
2023-06-28 10:09:53 -0500

    add: CategoryResource.cs

---
+++ LookMedico.API/StoreInventoryManagement/Resources/CategoryResource.cs
@@ -0,0 +1,9 @@
+namespace LookMedico.API.Store_Inventory_Management.Resources;
+
+public class CategoryResource
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string PhotoUrl { get; set; }
+}
