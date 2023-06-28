8f47bb9af0af5aa6451010c613cc1c73764dd655
ValeryAyasta <102206366+ValeryAyasta@users.noreply.github.com>
2023-06-28 08:51:54 -0500

    fix: IDoctorService.cs

--- LookMedico.API/ProfilesManagement/Domain/Services/IDoctorService.cs
+++ LookMedico.API/ProfilesManagement/Domain/Services/IDoctorService.cs
@@ -1,13 +1,10 @@
-using LookMedico.API.ProfilesManagement.Domain.Models;
-using LookMedico.API.ProfilesManagement.Domain.Services.Communication;
-
-namespace LookMedico.API.ProfilesManagement.Domain.Services;
-
 public interface IDoctorService
 {
     Task<IEnumerable<Doctor>> ListAsync();
     //devuelve un enumerable de doctors
-    
+
+    Task<Doctor> GetByIdAsync(string id);
+
     Task<DoctorResponse> SaveAsync(Doctor doctor);
 
     Task<DoctorResponse> UpdateAsync(string id, Doctor doctor);
