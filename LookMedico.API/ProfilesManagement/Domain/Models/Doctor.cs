55caed413f14bdf263a39ba7df853b6937ec451e
ValeryAyasta <102206366+ValeryAyasta@users.noreply.github.com>
2023-06-28 08:47:39 -0500

    fix: Doctor.cs

--- LookMedico.API/ProfilesManagement/Domain/Models/Doctor.cs
+++ LookMedico.API/ProfilesManagement/Domain/Models/Doctor.cs
@@ -9,10 +9,8 @@ public class Doctor
     public string LastName { get; set; }
     
     public string Email { get; set; }
-    
-    public string Gender { get; set; }
-    
-    public long Phone { get; set; }
+
+    public string Phone { get; set; }
     
     public string Address { get; set; }
     
